069578cfb8c1f679b57df556105488afe13646f2
showkhin1997 <showkhin15@gmail.com>
2023-05-31 10:03:47 +0600

    total cost

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // button function and costing
-function memoryAcceccories(buttonId) {
+function acceccoriesCost(buttonId) {
     if (buttonId == '8gb-unified-memory') {
         document.getElementById('extra-memory-cost').innerText = 0;
     }
@@ -24,27 +24,44 @@ function memoryAcceccories(buttonId) {
 
 }
 
+// total cost
+function totalCost() {
+    const bestPrice = document.getElementById('best-price').innerText;
+    const extraMemoryCost = document.getElementById('extra-memory-cost').innerText;
+    const extraStorageCost = document.getElementById('extra-storage-cost').innerText;
+    const delivaryCost = document.getElementById('delivery-charge-cost').innerText;
+    const totalCost = parseInt(bestPrice) + parseInt(extraMemoryCost) + parseInt(extraStorageCost) + parseInt(delivaryCost);
+    document.getElementById('total-Cost').innerText = totalCost;
+}
+
 // memory accessories button
 document.getElementById('8gb-unified-memory').addEventListener('click', function () {
-    memoryAcceccories('8gb-unified-memory');
+    acceccoriesCost('8gb-unified-memory');
+    totalCost();
 });
 document.getElementById('16gb-unified-memory').addEventListener('click', function () {
-    memoryAcceccories('16gb-unified-memory');
+    acceccoriesCost('16gb-unified-memory');
+    totalCost();
 });
 // storage accessories button
 document.getElementById('256gb-ssd-storage').addEventListener('click', function () {
-    memoryAcceccories('256gb-ssd-storage');
+    acceccoriesCost('256gb-ssd-storage');
+    totalCost();
 });
 document.getElementById('512gb-ssd-storage').addEventListener('click', function () {
-    memoryAcceccories('512gb-ssd-storage');
+    acceccoriesCost('512gb-ssd-storage');
+    totalCost();
 });
 document.getElementById('1tb-ssd-storage').addEventListener('click', function () {
-    memoryAcceccories('1tb-ssd-storage');
+    acceccoriesCost('1tb-ssd-storage');
+    totalCost();
 });
 // delivery cost
 document.getElementById('free-delivery-cost').addEventListener('click', function () {
-    memoryAcceccories('free-delivery-cost')
+    acceccoriesCost('free-delivery-cost');
+    totalCost();
 });
 document.getElementById('paid-delivery-cost').addEventListener('click', function () {
-    memoryAcceccories('paid-delivery-cost')
+    acceccoriesCost('paid-delivery-cost');
+    totalCost();
 });
